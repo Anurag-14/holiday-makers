fc56d65b63b2789c334ea281e1e904d76c58ca8b
Anurag Tiwari <anuragt@techepoch.com>
2022-08-13 23:44:46 +0530

    first level code base added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { ThemeProvider } from 'emotion-theming'
+import { Global } from '@emotion/core'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import App from './containers/App'
+import * as serviceWorker from './serviceWorker'
+
+import { globalCSS, theme } from './styles/'
+
+ReactDOM.render(
+  <>
+    <Global styles={globalCSS} />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </>,
+  document.getElementById('root')
+)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()
